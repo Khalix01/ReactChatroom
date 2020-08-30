e2046e71d13c1003f92d11ae7c30a7c88caf2e61
khalix <krutikshah01@gmail.com>
2020-08-30 12:46:56 -0400

    Updates

---
+++ precache-manifest.a14eb4c5f1f1c9061b3c4b1567895e4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d5156801699a123a40afafc0a252d5",
+    "url": "/ReactChatroom/index.html"
+  },
+  {
+    "revision": "a21e0633d7c95907f56b",
+    "url": "/ReactChatroom/static/css/main.ff68c1a5.chunk.css"
+  },
+  {
+    "revision": "40f3f36c2cd83a78bc35",
+    "url": "/ReactChatroom/static/js/2.fad881da.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/ReactChatroom/static/js/2.fad881da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21e0633d7c95907f56b",
+    "url": "/ReactChatroom/static/js/main.ad223904.chunk.js"
+  },
+  {
+    "revision": "3bbdda055d6c7dcb4da2",
+    "url": "/ReactChatroom/static/js/runtime-main.71fc4a85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactChatroom/static/media/logo.5d5d9eef.svg"
+  }
+]);
